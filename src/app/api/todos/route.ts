498fc245611dfac13ea2be347e619ecd8e4552e2
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-07-11 23:35:02 +0300

    adds a dynamic route

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,79 @@
+import { NextResponse } from "next/server";
+
+const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos';
+const API_KEY: string = process.env.DATA_API_KEY as string;
+
+//GET REQUEST
+export async function GET() {
+    const res = await fetch(DATA_SOURCE_URL);
+
+    const todos: Todo[] = await res.json();
+
+    return NextResponse.json(todos);
+
+}
+
+//POST REQUEST
+export async function POST(request: Request) {
+    const { userId, title }: Partial<Todo> = await request.json();
+
+    if (!userId || !title) return NextResponse.json({'message':'missing required data'});
+
+    const res = await fetch(DATA_SOURCE_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': API_KEY
+        },
+        body: JSON.stringify({
+            userId, title, completed: false
+        })
+    });
+
+    const newTodo: Todo = await res.json();
+
+    return NextResponse.json(newTodo)
+ }
+
+
+ //PUT REQUEST
+export async function PUT(request: Request) {
+    const { userId, id,  title, completed }: Todo = await request.json();
+
+    if (!userId || !id || !title || typeof(completed) !== 'boolean') return NextResponse.json({'message':'missing required data'});
+
+    const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': API_KEY
+        },
+        body: JSON.stringify({
+            userId, title, completed
+        })
+    });
+
+    const updatedTodo: Todo = await res.json();
+
+    return NextResponse.json(updatedTodo)
+ }
+
+ 
+ //DELETE REQUEST
+export async function DELETE(request: Request) {
+    const { id }: Partial<Todo> = await request.json();
+
+    if (!id) return NextResponse.json({'message':'todo id is required'});
+
+    await fetch(`${DATA_SOURCE_URL}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': API_KEY
+        }
+    })
+
+    return NextResponse.json({'message' : `Todo ${id} is deleted`})
+ }
+
+
